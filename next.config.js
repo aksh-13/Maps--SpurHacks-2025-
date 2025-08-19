de0a0ce985aacbbc091ed985035b55c9d250bc48
aksh-13 <73313866+aksh-13@users.noreply.github.com>
2025-08-19 16:12:15 -0400

    netlify

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "a0.muscache.com"],
+    domains: ["images.unsplash.com", "a0.muscache.com", "images.ticketmaster.com"],
     unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
-  },
-  // Environment variables are automatically available in Vercel
-  // No need to explicitly define them in next.config.js
-  experimental: {
-    // Optimize for Vercel deployment
   },
 };
 
